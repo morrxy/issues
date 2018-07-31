61cea394852ebc1ca6810ed170d7981c9bbed9f3
morrxy <morrxy@gmail.com>
2018-07-31 11:03:43 +0800

    add doc test

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,5 +1,16 @@
 defmodule Issues.TableFormatter do
   import Enum, only: [each: 2, map: 2, map_join: 3, max: 1]
+  
+  @doc """
+  Takes a list of row data, where each row is 
+  a Map, and a list of headers.
+  Prints a table to STDOUT of the data fro each 
+  row identified by each header. That is, each 
+  header identifies a column, and those columns 
+  are extraceded and printed from the rows.
+  We calculate the width of each column to fit 
+  the longest element in that column.
+  """
 
   def print_table_for_columns(rows, headers) do
     with data_by_columns = split_into_columns(rows, headers),
@@ -12,6 +23,22 @@ defmodule Issues.TableFormatter do
       # IO.inspect(data_by_columns)
     end
   end
+  
+  @doc ~S"""
+  Given a list of rows, where each row contains a
+  keyed list of columns, return a list containing
+  lists of the data in each column. The `headers`
+  parameter contains the list of columns to extract
+  
+  ## Example
+  
+      iex> list = [Enum.into([{"a", "1"},{"b", "2"},
+      {"c", "3"}], %{}), Enum.into([{"a", "4"},{"b", "5"},
+      {"c", "6"}], %{})]
+      iex> Issues.TableFormatter.split_into_columns(list, ["a", "b", "c"])
+      [["1", "4"], ["2", "5"], ["3", "6"]]
+      
+  """
 
   def split_into_columns(rows, headers) do
     for header <- headers do
